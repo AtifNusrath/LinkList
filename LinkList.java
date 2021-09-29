138a50201a2b9e4715c5b9a6f08710b8e36280cf
atif00 <mohammedatif90@gmail.com>
2021-09-29 13:42:21 +0530

    ADD simple linked list

--- LinkList.java
+++ LinkList.java
@@ -1,59 +1,24 @@
 package com.bridgelabz;
 
+class Node<T> {
+    T data;
+    Node<T> next;
+}
+
 public class LinkList<T> {
-    private LinkedNode<T> first;
-    private LinkedNode<T> last;
-    private int count;
-
-
-    private class LinkedNode<T> {
-        private T data;
-        private LinkedNode<T> next;
-
-        public LinkedNode() {
-            this.data = null;
-            this.next = null;
-        }
-
-        public LinkedNode(T obj) {
-            this.data = obj;
-            this.next = null;
-        }
-
-        public T getData() {
-            return data;
-        }
-
-        public void setData(T data) {
-            this.data = data;
-        }
-
-        public LinkedNode<T> getNext() {
-            return next;
-        }
-
-        public void setNext(LinkedNode<T> next) {
-            this.next = next;
-        }
-
-    }
-
-    public LinkList() {
-        LinkedNode<T> newLiked = new LinkedNode<T>();
-        this.first = newLiked;
-        this.last = this.first;
-    }
-
+    Node<T> head;
     public void add(T data) {
-        LinkedNode<T> newData = new LinkedNode<T>(data);
-        if (this.first.getData() == null) {
-            this.first = newData;
-            this.last = this.first;
+        Node<T> new_node = new Node<T>();
+        new_node.data = data;
+        if (head == null) {
+            head = new_node;
         } else {
-            this.last.setNext(newData);
-            this.last = newData;
+            Node<T> n = head;
+            while (n.next != null) {
+                n = n.next;
+            }
+            n.next = new_node;
         }
-        count++;
     }
 
     public static void main(String[] args) {
@@ -61,5 +26,14 @@ public class LinkList<T> {
         linkedLst.add(56);
         linkedLst.add(30);
         linkedLst.add(70);
+        //linkedLst.show();
+    }
+    public void show() {
+        Node<T> n = head;
+        while (n.next != null) {
+            System.out.print(n.data+"->");
+            n = n.next;
+        }
+        System.out.println(n.data);
     }
 }
