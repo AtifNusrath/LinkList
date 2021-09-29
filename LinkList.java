f59124457e27fd8b838a9cc0a181a59a7ba73abc
atif00 <mohammedatif90@gmail.com>
2021-09-29 14:18:48 +0530

    ADD delete first element in linked list

--- LinkList.java
+++ LinkList.java
@@ -21,13 +21,36 @@ public class LinkList<T> {
         }
     }
 
-    public static void main(String[] args) {
-        LinkList<Integer> linkedLst = new LinkList<>();
-        linkedLst.add(56);
-        linkedLst.add(30);
-        linkedLst.add(70);
-        //linkedLst.show();
+    public void insertAt(int index, T data) {
+        Node<T> new_node = new Node<T>();
+        new_node.data = data;
+        new_node.next = null;
+
+        if (index == 0) {
+            insertAtStart(data);
+        } else {
+            Node<T> n = head;
+            for (int i = 0; i < index - 1; i++) {
+                n = n.next;
+            }
+            new_node.next = n.next;
+            n.next = new_node;
+        }
     }
+    public void insertAtStart(T data) {
+        Node<T> new_node = new Node<T>();
+        new_node.data = data;
+        new_node.next = null;
+        new_node.next = head;
+        head = new_node;
+    }
+    public void deleteAtStart() {
+        Node<T> n = head;
+        head = head.next;
+        System.out.println("Node deleted is: " + n.data);
+        n = null;
+    }
+
     public void show() {
         Node<T> n = head;
         while (n.next != null) {
@@ -36,4 +59,15 @@ public class LinkList<T> {
         }
         System.out.println(n.data);
     }
+    public static void main(String[] args) {
+        LinkList<Integer> linkedLst = new LinkList<>();
+        linkedLst.add(56);
+        linkedLst.add(70);
+        linkedLst.show();
+        linkedLst.insertAt(1,30);
+        linkedLst.show();
+        linkedLst.deleteAtStart();
+        linkedLst.show();
+    }
+
 }
